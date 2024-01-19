52d89a39af225d71227a5fedf96855260c2f8afe
destrada-2019308 <destrada-2019308@kinal.edu.gt>
2024-01-19 09:18:38 -0600

    Quinto commit

--- code.js
+++ code.js
@@ -3,6 +3,8 @@ function decision ()
     var array = ["agua", "vaso", "asd", "juan"];
     var aleatorio=Math.floor((Math.random()*4));
     console.log(array[aleatorio]);
+    
+    //document.getElementById("mostrar").innerHTML = (array[aleatorio])
 }
 
 decision();
@@ -10,6 +12,14 @@ decision();
 const getValueInput = () =>{
     let valorInput = document.getElementById("obtener").value;
     console.log(valorInput);
+}
+
+
+
+const validarLetra = () =>{
+    var btn = document.getElementById("boton");
+    console(btn);
+
 }
 
 var c = document.getElementById("canvas");

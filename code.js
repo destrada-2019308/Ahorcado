d2025ead1303c2a901e60e4bcc39b143f1c424df
destrada-2019308 <destrada-2019308@kinal.edu.gt>
2024-01-19 08:59:21 -0600

    Cuarto commit, inicio de palabras

--- code.js
+++ code.js
@@ -1,16 +1,16 @@
-/*const random = (num) =>{
-
-    const array = ["asd","qwe"];
-    console.log(array);
-
-    //array(Math.floor(Math.random() * 10));
-    let result1 = Math.random().array.substring(0, num);
-
-    return result1;
+function decision ()
+{
+    var array = ["agua", "vaso", "asd", "juan"];
+    var aleatorio=Math.floor((Math.random()*4));
+    console.log(array[aleatorio]);
 }
 
-console.log(random(3))
-*/
+decision();
+
+const getValueInput = () =>{
+    let valorInput = document.getElementById("obtener").value;
+    console.log(valorInput);
+}
 
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");

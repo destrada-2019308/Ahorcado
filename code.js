e5f48a1a8cfae04aa6ef380005386177a7f38dbb
destrada-2019308 <destrada-2019308@kinal.edu.gt>
2024-01-19 08:26:58 -0600

    Tercer commit

--- code.js
+++ code.js
@@ -1,9 +1,20 @@
+/*const random = (num) =>{
 
+    const array = ["asd","qwe"];
+    console.log(array);
 
+    //array(Math.floor(Math.random() * 10));
+    let result1 = Math.random().array.substring(0, num);
+
+    return result1;
+}
+
+console.log(random(3))
+*/
 
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 ctx.beginPath();
-ctx.moveTo(0,0);
+ctx.moveTo(25,25);
 ctx.lineTo(50,50);
 ctx.stroke();
